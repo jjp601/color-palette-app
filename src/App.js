ddfdb46b36eb4fc202546b353280fc1a538ba99a
Jon Powell <jpowell68601@gmail.com>
2021-03-14 19:11:52 -0500

    Setup and style Color Slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Palette from './components/Palette';
 import seedColors from './utils/seedColors';
+import { generatePalette } from './utils/colorHelpers';
 
 const App = () => {
   return (
     <div>
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
