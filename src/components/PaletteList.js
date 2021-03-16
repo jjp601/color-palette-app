cd9fc32b14870f7bf5c245c97174e1c58d91902f
Jon Powell <jpowell68601@gmail.com>
2021-03-16 16:44:16 -0500

    Setup styles with JSS for MiniPalette and PaletteList

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,20 +1,80 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalatte from './MiniPalette';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+    root: {
+        height: "100vh",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        /* background by SVGBackgrounds.com */
+        backgroundColor: "#394bad",
+        // backgroundImage: `url(${bg})`,
+        overflow: "scroll"
+      },
+      heading: {
+        fontSize: "2rem"
+      },
+      container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        // [sizes.down("xl")]: {
+        //   width: "80%"
+        // },
+        // [sizes.down("xs")]: {
+        //   width: "75%"
+        // }
+      },
+      nav: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        color: "white",
+        "& a": {
+          color: "white"
+        }
+      },
+      palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "2.5rem",
+        // [sizes.down("md")]: {
+        //   gridTemplateColumns: "repeat(2, 50%)"
+        // },
+        // [sizes.down("xs")]: {
+        //   gridTemplateColumns: "repeat(1, 100%)",
+        //   gridGap: "1.4rem"
+        // }
+      }
+}
 
 class PaletteList extends Component {
     render() {
-        const { palettes } = this.props;
+        const { palettes, classes } = this.props;
         return (
-            <div>
-                <h1>Color Palette</h1>
-                {palettes.map((palette) => (
-                    <p>
-                        <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                    </p>
-                ))}
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>Color Palette</h1>
+                    </nav>
+                    <div className={classes.palettes}>
+                        {palettes.map((palette) => (
+                            <MiniPalatte {...palette} />
+                        ))}
+                    </div>
+                    
+                </div>
             </div>
         );
     }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
