8d1cba6c9826dd036fcf121af08862c3c3dcaad1
Jon Powell <jpowell68601@gmail.com>
2021-03-17 22:31:44 -0500

    Update Colorbox

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -36,7 +36,10 @@ class ColorBox extends Component {
                         </div>
                         <button className="copy-button">Copy</button>
                     </div>
-                    <Link to={`/palette/${paletteId}/${id}`} onClick={(e) => e.stopPropagation()}>
+                    <Link 
+                        to={`/palette/${paletteId}/${id}`} 
+                        onClick={(e) => e.stopPropagation()}
+                    >
                         <span className="see-more">More</span>
                     </Link>
                     
