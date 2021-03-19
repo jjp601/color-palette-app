04cf0fc34f80cc3e5ff353ce9e6b14957a3aca82
Jon Powell <jpowell68601@gmail.com>
2021-03-18 22:46:25 -0500

    Refactor PaletteList component

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,86 +1,101 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import MiniPalatte from './MiniPalette';
-import { withStyles } from '@material-ui/styles';
-
-const styles = {
-    root: {
-        height: "100vh",
-        display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "center",
-        /* background by SVGBackgrounds.com */
-        backgroundColor: "#394bad",
-        // backgroundImage: `url(${bg})`,
-        overflow: "scroll"
-      },
-      heading: {
-        fontSize: "2rem"
-      },
-      container: {
-        width: "50%",
-        display: "flex",
-        alignItems: "flex-start",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        // [sizes.down("xl")]: {
-        //   width: "80%"
-        // },
-        // [sizes.down("xs")]: {
-        //   width: "75%"
-        // }
-      },
-      nav: {
-        display: "flex",
-        width: "100%",
-        justifyContent: "space-between",
-        alignItems: "center",
-        color: "white",
-        "& a": {
-          color: "white"
-        }
-      },
-      palettes: {
-        boxSizing: "border-box",
-        width: "100%",
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "2.5rem",
-        // [sizes.down("md")]: {
-        //   gridTemplateColumns: "repeat(2, 50%)"
-        // },
-        // [sizes.down("xs")]: {
-        //   gridTemplateColumns: "repeat(1, 100%)",
-        //   gridGap: "1.4rem"
-        // }
-      }
-}
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import Dialog from "@material-ui/core/Dialog";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { withStyles } from "@material-ui/styles";
+import MiniPalette from "./MiniPalette";
+import blue from "@material-ui/core/colors/blue";
+import red from "@material-ui/core/colors/red";
+import styles from "./styles/PaletteListStyles";
 
 class PaletteList extends Component {
-
-
-    goToPalette(id) {
-      this.props.history.push(`/palette/${id}`)
-    }
-
-    render() {
-      const { palettes, classes } = this.props;
-      return (
-          <div className={classes.root}>
-              <div className={classes.container}>
-                  <nav className={classes.nav}>
-                      <h1>Color Palette</h1>
-                  </nav>
-                  <div className={classes.palettes}>
-                      {palettes.map((palette) => (
-                          <MiniPalatte {...palette} handleClick={() => this.goToPalette(palette.id)} />
-                      ))}
-                  </div>
-                  
-              </div>
-          </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      openDeleteDialog: false,
+      deletingId: ""
+    };
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.goToPalette = this.goToPalette.bind(this);
+  }
+  openDialog(id) {
+    this.setState({ openDeleteDialog: true, deletingId: id });
+  }
+  closeDialog() {
+    this.setState({ openDeleteDialog: false, deletingId: "" });
+  }
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
+  }
+  handleDelete() {
+    this.props.deletePalette(this.state.deletingId);
+    this.closeDialog();
+  }
+  render() {
+    const { palettes, classes } = this.props;
+    const { openDeleteDialog } = this.state;
+    return (
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1 className={classes.heading}>Color Palette</h1>
+            <Link to='/palette/new'>Create Palette</Link>
+          </nav>
+          <TransitionGroup className={classes.palettes}>
+            {palettes.map(palette => (
+              <CSSTransition key={palette.id} classNames='fade' timeout={500}>
+                <MiniPalette
+                  {...palette}
+                  goToPalette={this.goToPalette}
+                  openDialog={this.openDialog}
+                  key={palette.id}
+                  id={palette.id}
+                />
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </div>
+        <Dialog
+          open={openDeleteDialog}
+          aria-labelledby='delete-dialog-title'
+          onClose={this.closeDialog}
+        >
+          <DialogTitle id='delete-dialog-title'>
+            Delete This Palette?
+          </DialogTitle>
+          <List>
+            <ListItem button onClick={this.handleDelete}>
+              <ListItemAvatar>
+                <Avatar
+                  style={{ backgroundColor: blue[100], color: blue[600] }}
+                >
+                  <CheckIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary='Delete' />
+            </ListItem>
+            <ListItem button onClick={this.closeDialog}>
+              <ListItemAvatar>
+                <Avatar style={{ backgroundColor: red[100], color: red[600] }}>
+                  <CloseIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary='Cancel' />
+            </ListItem>
+          </List>
+        </Dialog>
+      </div>
+    );
+  }
 }
-
 export default withStyles(styles)(PaletteList);
