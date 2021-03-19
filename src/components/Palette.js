8b7ec3df45b7cd16276905d375ba45191e058202
Jon Powell <jpowell68601@gmail.com>
2021-03-18 22:42:28 -0500

    Refactor Palette and PaletteFooter styles

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,62 +1,48 @@
-import React, { Component } from 'react';
-import ColorBox from './ColorBox';
-import Navbar from './Navbar';
-import PaletteFooter from './PaletteFooter';
-import '../styles/Palette.css';
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
+import { withStyles } from "@material-ui/styles";
+import styles from "./styles/PaletteStyles";
 
 class Palette extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            level: 500,
-            format: 'hex'
-        }
-
-        this.changeLevel = this.changeLevel.bind(this)
-        this.changeFormat = this.changeFormat.bind(this)
-    }
-
-    changeLevel(level) {
-        this.setState(() => ({
-            level
-        }))
-    }
-
-    changeFormat(value) {
-        this.setState(() => ({
-            format: value
-        }))
-    }
-
-    render() {
-        const { colors, paletteName, emoji, id } = this.props.palette;
-        const { level, format } = this.state;
-        const colorBoxes = colors[level].map((color) => (
-            <ColorBox 
-                background={color[format]} 
-                name={color.name} 
-                key={color.id} 
-                id={color.id} 
-                paletteId={id}
-                showLink
-            />
-        ))
-        
-        return (
-            <div className="Palette">
-                <Navbar 
-                    level={level} 
-                    changeLevel={this.changeLevel}
-                    handleChange={this.changeFormat}
-                    showingAllColors
-                />
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-                <PaletteFooter paletteName={paletteName} emoji={emoji} />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { level: 500, format: "hex" };
+    this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+  changeLevel(level) {
+    this.setState({ level });
+  }
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
+  render() {
+    const { colors, paletteName, emoji, id } = this.props.palette;
+    const { classes } = this.props;
+    const { level, format } = this.state;
+    const colorBoxes = colors[level].map(color => (
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        key={color.id}
+        moreUrl={`/palette/${id}/${color.id}`}
+        showingFullPalette
+      />
+    ));
+    return (
+      <div className={classes.Palette}>
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+          showingAllColors
+        />
+        <div className={classes.colors}>{colorBoxes}</div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
+      </div>
+    );
+  }
 }
-
-export default Palette;
+export default withStyles(styles)(Palette);
