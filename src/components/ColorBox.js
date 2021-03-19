3fcdc66bc9761ab39df964d24fc7bf7fff4e176e
Jon Powell <jpowell68601@gmail.com>
2021-03-18 22:31:40 -0500

    Setup Color Picker Form component

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,57 +1,64 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import chroma from 'chroma-js';
-import '../styles/ColorBox.css';
+import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/styles";
+import styles from "./styles/ColorBoxStyles";
 
 class ColorBox extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            copied: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
 
-        this.changeCopyState = this.changeCopyState.bind(this)
-    }
+  render() {
+    const {
+      name,
+      background,
+      moreUrl,
+      showingFullPalette,
+      classes
+    } = this.props;
 
-    changeCopyState () {
-        this.setState({ copied: true }, () => {
-            setTimeout(() => this.setState({ copied: false}), 1500);
-        })
-    }
+    const { copied } = this.state;
+    return (
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div style={{ background }} className={classes.ColorBox}>
+          <div
+            style={{ background }}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied
+            })}
+          />
 
-    render() {
-        const { name, background, paletteId, id, showLink } = this.props;
-        const { copied } = this.state;
-        const isDarkColor = chroma(background).luminance() <= 0.05;
-        
-        return (
-            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-                <div style={{ background }} className="ColorBox">
-                    <div style={{ background }} className={`copy-overlay ${copied && 'show'}`}></div>
-                    <div className={`copy-msg ${copied && 'show'}`}>
-                        <h1>Copied!</h1>
-                        <p>{this.props.background}</p>
-                    </div>
-                    <div className="copy-container">
-                        <div className="box-content">
-                            <span>{name}</span>
-                        </div>
-                        <button className="copy-button">Copy</button>
-                    </div>
-                    {showLink && (
-                        <Link 
-                            to={`/palette/${paletteId}/${id}`} 
-                            onClick={(e) => e.stopPropagation()}
-                        >
-                            <span className="see-more">More</span>
-                        </Link>
-                    )}
-                    
-                </div>
-            </CopyToClipboard>
-        );
-    }
+          <div
+            className={classNames(classes.copyMessage, {
+              [classes.showMessage]: copied
+            })}
+          >
+            <h1>copied!</h1>
+            <p className={classes.copyText}>{background}</p>
+          </div>
+          <div>
+            <div className={classes.boxContent}>
+              <span className={classes.colorName}>{name}</span>
+            </div>
+            <button className={classes.copyButton}>Copy</button>
+          </div>
+          {showingFullPalette && (
+            <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+              <span className={classes.seeMore}>MORE</span>
+            </Link>
+          )}
+        </div>
+      </CopyToClipboard>
+    );
+  }
 }
-
-export default ColorBox;
+export default withStyles(styles)(ColorBox);
